816a46099e1719315075c8c1b63aa1a50e5c8d49
Roman <romankos1993@gmail.com>
2022-10-18 19:29:37 +0300

    homework 003(38) completed

---
+++ homework003(38)/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+void Main()
+{
+    Console.Clear();
+
+    Console.WriteLine("Enter size array:");
+    int num = int.Parse(Console.ReadLine()!);
+
+    double[] ourArray = FillArray(num);
+    double max1 = FindMax(ourArray);
+    double min1 = FindMin(ourArray);
+    Console.WriteLine($"Max number = {max1}, min number = {min1}");
+    Console.WriteLine($"Difference between maximum and minimum number = {Math.Round((max1 - min1), 2)}");
+}
+double[] FillArray(int size)
+{
+    double[] arr = new double[size];
+    Console.WriteLine("Array:");
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = Convert.ToDouble(new Random().Next(100, 1000)) / 100;
+        Console.Write($"{arr[i]} ");
+    }
+
+    return arr;
+}
+double FindMax(double[] arr)
+{
+    double max = Int32.MinValue;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+double FindMin(double[] arr)
+{
+    double min = Int32.MaxValue;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+    return min;
+}
+
+Main();
